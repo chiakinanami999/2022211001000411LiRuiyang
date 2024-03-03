c73b0fb9861b6a269e675b78d509143962cf5394
chiakinanami999 <chiakinanami99999@gmail.com>
2024-03-03 21:35:46 +0800

    First Commit

---
+++ src/main/java/com/example/LiRuiyang2022211001000411/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.LiRuiyang2022211001000411;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
