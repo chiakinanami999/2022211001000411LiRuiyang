4ef6bc76f951ba54f28cc987abab8d9affc5acb0
chiakinanami999 <chiakinanami99999@gmail.com>
2024-03-28 18:19:40 +0800

    HW-Week 3 Commit

--- src/main/java/com/liruiyang/week3/demo/RegisterServlet.java
+++ src/main/java/com/liruiyang/week3/demo/RegisterServlet.java
@@ -1,31 +1,108 @@
 package com.liruiyang.week3.demo;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Set;
 
-public class RegisterServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/register")
+
+public class RegisterServlet<set, OrderList> extends HttpServlet {
+
+    Connection con = null;
+
+    @Override
+    public void init() throws ServletException {
+        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        String url = "jdbc:sqlserver://localhost;databaseName=userdb;";
+        String username = "sa";
+        String password = "admin123456789";
+
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
+//        String id1="1";
+//        String username1 = "LiRuiyang";
+//        String password1 = "2022211001000411";
+//        String email1 = "123@qq.com";
+//        String gender1 = "male";
+//        String birthdate1 = "1212-12-12";
+//
+//        try {
+//            SET IDENTITY_INSERT [ database.[ owner.] ] { table } { ON | OFF }
+//            set identity_insert userdb ON;
+//            PreparedStatement stmt = con.prepareStatement("insert into usertable(id,username,password,email,gender,birthdate)  values(?,?,?,?,?,?)");
+//            stmt.setString(1, id1);
+//            stmt.setString(2, username1);
+//            stmt.setString(3, password1);
+//            stmt.setString(4, email1);
+//            stmt.setString(5, gender1);
+//            stmt.setString(6, birthdate1);
+//
+//            stmt.executeUpdate();
+//            con.commit();
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        String str = "select * from usertable";
+        try {
+            ResultSet rs = con.createStatement().executeQuery(str);
+            try {
+                PrintWriter writer = response.getWriter();
+                writer.println("<html><head><meta charset=\"UTF-8\"><title>User Information</title></head><body>");
+                writer.println("<table border='1'>");
+                writer.println("<tr><th>ID</th><th>Username</th><th>Password</th><th>Email</th><th>Gender</th><th>Birthdate</th></tr>");
+                while (rs.next()) {
+                    String id = rs.getString("id");
+                    String username = ((ResultSet) rs).getString("username");
+                    String password = rs.getString("password");
+                    String email = rs.getString("email");
+                    String gender = rs.getString("gender");
+                    String birthdate = rs.getString("birthdate");
+                    writer.println("<tr>");
+                    writer.println("<td>" + id + "</td>");
+                    writer.println("<td>" + username + "</td>");
+                    writer.println("<td>" + password + "</td>");
+                    writer.println("<td>" + email + "</td>");
+                    writer.println("<td>" + gender + "</td>");
+                    writer.println("<td>" + birthdate + "</td>");
+                    writer.println("</tr>");
+                }
+                writer.println("</table>");
+                writer.println("</body></html>");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        String email = request.getParameter("email");
-        String gender = request.getParameter("gender");
-        String birthDate = request.getParameter("birthDate");
 
-        PrintWriter writer = response.getWriter();
-        writer.println("<br> username :"+username);
-        writer.println("<br> password :"+password);
-        writer.println("<br> email :"+email);
-        writer.println("<br> gender :"+gender);
-        writer.println("<br> Birth Date :"+birthDate);
-        writer.close();
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
--- src/main/webapp/register.jsp
+++ src/main/webapp/register.jsp
@@ -9,26 +9,32 @@
 <html>
 <head>
     <title>Register JSP page</title>
-    <style>
-        h1 div {
-            text-align: center;
-        }
-
-    </style>
 </head>
 <body>
-    <h1>New User Registration</h1>
-    <div>
-        <form method="post" action="register">
-            username<input type="text" name="username"/><br/>
-            ID<input type="text" name="password"/><br/>
-            email<input type="text" name="email"/><br/>
-            Gender: <input type="radio" name="gender" value="Male">Male <input type="radio" name="gender" value="Female">Female<br/>
-            <input type="text" name="birthDate"><br/>
-            <input type="submit" value="Register"/>
-        </form>
+<h2>New User Registration!</h2>
+<form id="registrationForm"  method="post" action="/register">
 
-    </div>
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" placeholder="Username" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" placeholder="password" required><br>
+
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" placeholder="Email" required><br>
+
+    <label>Gender:</label>
+    <input type="radio" id="genderMale" name="gender" value="Male" required>
+    <label for="genderMale">Male</label>
+
+    <input type="radio" id="genderFemale" name="gender" value="Female" required>
+    <label for="genderFemale">Female</label><br>
+
+    <label for="birthdate">Birthdate:</label>
+    <input type="text" id="birthdate" name="birthdate" placeholder="Date of Birth(yyyy-mm-dd)" required ><br>
+
+    <input type="submit" value="Register" style="background-color: orange; color: white;width: 120px;height: 30px;margin-left: 50px;margin-top: 10px">
+</form>
 
 </body>
 </html>
